d2dc6997696bffc81640dc71b2a50a8c3382c6a2
jtisaac <jtisaac@NN123-HA-07.HS.sd203.org>
2015-09-15 15:09:50 -0500

    Started the door project
    Door project but no test yet

---
+++ Door/Door.java
@@ -0,0 +1,130 @@
+
+
+/**
+ * Write a description of class Door here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Door
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private String name; // name of door
+    private String state; // open or closed
+
+
+    {
+        // initialise instance variables
+        //private String name;
+        this.state = state;
+        this.name = name;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *    that describes the operation of the method
+     *
+     * @pre        preconditions for the method
+     *            (what the method assumes about the method's parameters and class's state)
+     *            
+     * @post    postconditions for the method
+     *            (what the method guarantees upon completion)
+     *            Door will be open
+     * @param   state of door
+     * @return    description of the return value
+     */
+    public void open()
+    {
+        // put your code here
+        state = "Open";
+        //do not return
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     *          door will be closed
+     * @param   atate
+     * @return  description of the return value
+     */
+    public void close()
+    {
+        // put your code here
+        state = "Closed";
+        //return Door.state;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getState()
+    {
+        // put your code here
+        return this.state;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public String getName()
+    {
+        // put your code here
+        return this.name;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void setName(String newName)
+    {
+        // put your code here
+        this.name = newName;
+    }
+    
+    /**
+     * An example of a method - replace this comment with your own
+     *  that describes the operation of the method
+     *
+     * @pre     preconditions for the method
+     *          (what the method assumes about the method's parameters and class's state)
+     * @post    postconditions for the method
+     *          (what the method guarantees upon completion)
+     * @param   y   description of parameter y
+     * @return  description of the return value
+     */
+    public void setState(String newState)
+    {
+        // put your code here
+        this.state = newState;
+    }
+    
+}
